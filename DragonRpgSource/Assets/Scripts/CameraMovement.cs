ed8267fef8dbd334fdc6075f7107205fb784e6e4
fykus <gulida_mikhail@mail.ru>
2025-07-06 15:31:17 +0300

    Create folder scripts

---
+++ DragonRpgSource/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I hope it s work");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
